d9ad4c31ff78aa809671a9ded31402d9ac3b1d6e
shivaprasad <kscswamy@gmail.com>
2016-01-07 00:31:04 -0800

    C-Sharp Basic level Projects
    These programs added above have some basic implementation of C-sharp
    coding .Involving arrays , different data types and many others.

---
+++ Array simple prgrm/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] simple = new int[] {5,8,89,7,77,55};
+           /*
+            simple[0]=22;
+            simple[1]=79;
+            simple[2]=789;
+
+            Console.WriteLine(simple[0].ToString(),simple[2].ToString());
+            Console.ReadLine();
+            
+
+            for(int i=0; i< 5; i++)
+            {
+                Console.WriteLine(simple[i].ToString());
+            }
+            Console.ReadLine();
+
+            string[] str = new string[] { "ramesh", "Suresh", "Harish", "Ganesh" };
+
+            foreach (string name in str )
+            {
+                Console.WriteLine(name);
+
+            }
+            Console.ReadLine();
+            */
+
+            string zig = "Hey mahn" + "This is a concatenation using + operator";
+
+            Console.WriteLine(zig.ToString());
+
+            char[] charArray = zig.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            foreach(char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Decisions in C#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hey Dude Write Something here:");
+            //string name;
+            //name = Console.ReadLine();
+            //Console.WriteLine("Welcome To C# Mr/Mrs:" + name);
+            //Console.ReadLine();
+
+            Console.WriteLine("Hey Bro Type numebers 1 ,2 or 3 To Win Some car;");
+            string enter;
+            enter = Console.ReadLine();
+
+            string message;
+
+            if (enter == "1")
+                message = "You won Lamborgini Garlado";
+            else if (enter == "2")
+                message = "You won Maruthi 800";
+            else if (enter == "3")
+                message = "You won i20 by Huyandi";
+            else
+                message = "Sorry !! Bro you didnt Win Any Gift try Next time";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
