df950b12b6db1ffda5d28b525306e70ae10b822d
shivaprasad <kscswamy@gmail.com>
2016-01-08 06:38:06 -0800

    WindowsApplication

---
+++ WindowsConsoleAppFiles/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplicationGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show("Hello World ,", "You are awesome!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+           MessageBox.Show("Hey a Basic Message!!");
+
+           // label1.Text("Hello You just Clicked a Button");
+        }
+    }
+}
