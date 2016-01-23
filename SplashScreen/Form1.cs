6d67e1ea32b45e765554bf63969629ec16452866
K SHIVAPRASAD <kscswamy@gmail.com>
2016-01-23 12:37:56 +0530

    Splash Screen Implementation

---
+++ SplashScreen/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace SplashScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+
+            Thread t = new Thread(new ThreadStart(SplashStrat));
+
+            t.Start();
+
+            Thread.Sleep(6000);
+
+            InitializeComponent();
+
+            t.Abort();
+
+
+        }
+
+        public void SplashStrat()
+        {
+            Application.Run(new Form2());
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha ha Main Work Space");
+        }
+    }
+}
